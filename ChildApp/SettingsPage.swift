fc830427d6b81c40c7bea5478a4177e13f89c8bc
alexandrshubskij <alexandrshubskij@gmail.com>
2019-12-06 02:07:14 +0300

    first commit with final design of app

---
+++ ChildApp/SettingsPage.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsPage.swift
+//  ChildApp
+//
+//  Created by Александр Шубский on 05.12.2019.
+//  Copyright © 2019 Александр Шубский. All rights reserved.
+//
+
+import UIKit
+
+class SettingsPage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
