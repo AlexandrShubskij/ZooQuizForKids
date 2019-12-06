0347626d19024d2af809360fe16433b116e05e5f
alexandrshubskij <alexandrshubskij@gmail.com>
2019-12-06 20:20:04 +0300

    some little changes in design

--- ChildApp/SettingsPage.swift
+++ ChildApp/SettingsPage.swift
@@ -10,9 +10,32 @@ import UIKit
 
 class SettingsPage: UIViewController {
 
+    @IBOutlet weak var soundLabel: UILabel!
+    @IBOutlet weak var soundSwitch: UISwitch!
+    
+    @IBOutlet weak var vibrationLabel: UILabel!
+    @IBOutlet weak var vibrationSwitch: UISwitch!
+    
+    
+    func elementsSettings(){
+        soundLabel.layer.borderColor = UIColor.brown.cgColor
+        soundLabel.layer.borderWidth = 3
+        soundSwitch.layer.cornerRadius = 15
+        soundSwitch.layer.borderColor = UIColor.brown.cgColor
+        soundSwitch.layer.borderWidth = 3
+        vibrationLabel.layer.borderColor = UIColor.brown.cgColor
+        vibrationLabel.layer.borderWidth = 3
+        vibrationSwitch.layer.cornerRadius = 15
+        vibrationSwitch.layer.borderColor = UIColor.brown.cgColor
+        vibrationSwitch.layer.borderWidth = 3
+        soundLabel.setRoundEdge()
+        vibrationLabel.setRoundEdge()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        elementsSettings()
+        
         // Do any additional setup after loading the view.
     }
     
--- ChildApp/StartPage.swift
+++ ChildApp/StartPage.swift
@@ -9,22 +9,68 @@
 import UIKit
 
 class StartPage: UIViewController {
-
+    
+    @IBOutlet weak var appNameLabel: UILabel!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var settingsButton: UIButton!
+    
+    func elementsSettings(){
+        appNameLabel.layer.cornerRadius = 5
+        appNameLabel.layer.borderColor = UIColor.brown.cgColor
+        appNameLabel.layer.borderWidth = 3
+        playButton.layer.cornerRadius = 15
+        playButton.layer.borderColor = UIColor.brown.cgColor
+        playButton.layer.borderWidth = 3
+        settingsButton.layer.cornerRadius = 15
+        settingsButton.layer.borderColor = UIColor.brown.cgColor
+        settingsButton.layer.borderWidth = 3
+        appNameLabel.setRoundEdge()
+    }
+    
+    func navBarSettings(){
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.isTranslucent = true
+        self.navigationController?.view.backgroundColor = UIColor.clear
+        
+        let backBarButtton = UIBarButtonItem(title: "НАЗАД", style: .plain, target: nil, action: nil)
+        navigationItem.backBarButtonItem = backBarButtton
+        
+        self.navigationItem.backBarButtonItem?.tintColor = UIColor.brown
+        self.navigationItem.backBarButtonItem?.setTitleTextAttributes([.font: UIFont(name: "CCRickVeitchW05", size: 40)!] , for: .normal)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        navBarSettings()
+        elementsSettings()
         // Do any additional setup after loading the view.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
+extension UILabel {
+
+    // extension user defined Method
+    func setRoundEdge() {
+        //How much the edge to be rounded
+        self.layer.cornerRadius = 15
+
+        
+        // Mask the bound
+        self.layer.masksToBounds = true
+        //clip the pixel contents
+        self.clipsToBounds = true
+    }
+}
+
